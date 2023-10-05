57d7cee744ae4344f4279c741172536c90863ef1
krlwbstr <44670608+w8krl@users.noreply.github.com>
2023-10-05 09:25:26 +0100

    Initial Commit

---
+++ pomodoro/pomodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  pomodoroApp.swift
+//  pomodoro
+//
+//  Created by K on 05/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct pomodoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
