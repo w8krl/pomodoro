47a8df03522cb98bc75f0014c5a00244bbb813d5
krlwbstr <44670608+w8krl@users.noreply.github.com>
2023-10-05 09:41:03 +0100

    First commit

--- pomodoro/ContentView.swift
+++ pomodoro/ContentView.swift
@@ -6,19 +6,54 @@
 //
 
 import SwiftUI
+import Combine
 
 struct ContentView: View {
+    @State private var timeRemaining: Int = 1500 // 25 minutes in seconds
+    @State private var timer: AnyCancellable?
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("\(timeRemaining / 60):\(String(format: "%02d", timeRemaining % 60))")
+                .font(.largeTitle)
+            
+            HStack {
+                Button("Start") {
+                    self.startTimer()
+                }
+                
+                Button("Stop") {
+                    self.stopTimer()
+                }
+            }
         }
-        .padding()
+    }
+    
+    func startTimer() {
+        // Invalidate any existing timer
+        timer?.cancel()
+        
+        timer = Timer.publish(every: 1, on: .main, in: .common)
+            .autoconnect()
+            .sink { _ in
+                if self.timeRemaining > 0 {
+                    self.timeRemaining -= 1
+                } else {
+                    // Pomodoro completed - you can add more logic here for breaks or notifications
+                    self.stopTimer()
+                }
+            }
+    }
+    
+    func stopTimer() {
+        timer?.cancel()
+        timer = nil
+        timeRemaining = 1500 // Resetting to 25 minutes
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
--- pomodoro/pomodoroApp.swift
+++ pomodoro/pomodoroApp.swift
@@ -6,12 +6,64 @@
 //
 
 import SwiftUI
+import AppKit
 
 @main
 struct pomodoroApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
-        WindowGroup {
+        Settings {
             ContentView()
         }
     }
 }
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    var window: NSWindow?
+    var statusBarItem: NSStatusItem!
+    var timeRemaining: Int = 1500
+    var timer: Timer?
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        statusBarItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        updateMenuBarTitle()
+        statusBarItem.button?.action = #selector(toggleTimer)
+    }
+
+    @objc func toggleTimer() {
+        if timer == nil {
+            startTimer()
+        } else {
+            stopTimer()
+        }
+    }
+
+    func startTimer() {
+        timer?.invalidate()
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
+    }
+
+    func stopTimer() {
+        timer?.invalidate()
+        timer = nil
+        timeRemaining = 1500
+        updateMenuBarTitle()
+    }
+
+    @objc func updateTime() {
+        if timeRemaining > 0 {
+            timeRemaining -= 1
+            updateMenuBarTitle()
+        } else {
+            stopTimer()
+        }
+    }
+
+    func updateMenuBarTitle() {
+        let minutes = timeRemaining / 60
+        let seconds = timeRemaining % 60
+        statusBarItem.button?.title = "\(minutes):\(String(format: "%02d", seconds))"
+    }
+}
+
